4052e342291a7fec88784ab9d29469a3708a88c0
Hzz <1240615807@qq.com>
2024-07-01 16:51:18 +0800

    feat: Refactor dtw class to use pointer parameters for faster computation

--- src/dtw.cu
+++ src/dtw.cu
@@ -11,7 +11,7 @@ __global__ void init_mGamma(double *gamma, int size, double value) {
     }
 }
 
-__global__ void compute_DTW(double*v ,double*w, double* mGamma, int wavefront, int constraint ,int N){
+__global__ void compute_DTW(const double*v ,const double*w, double* mGamma, int wavefront, int constraint ,int N){
     int i = blockIdx.x * blockDim.x + threadIdx.x; // 一维grid和一维block
 
     // 限制i的范围
@@ -21,23 +21,30 @@ __global__ void compute_DTW(double*v ,double*w, double* mGamma, int wavefront, i
     int j = wavefront - i;
     if(abs(i-j) > constraint  || j < 0 || j >= N) return;
 
+    // 使用共享内存
+    __shared__ double shared_v[512];
+    __shared__ double shared_w[512];
+    
+    shared_v[threadIdx.x] = v[i];
+    shared_w[threadIdx.x] = w[j];
+    __syncthreads();
+
     // printf("Thread [%d, %d]: i = %d, j = %d\n", blockIdx.x, threadIdx.x, i, j);
 
-    // mGamma[i][j] = fabs(v[i] - w[j]) + min(mGamma[i-1][j], mGamma[i][j-1], mGamma[i-1][j-1])
+    // 递推公式：mGamma[i][j] = fabs(v[i] - w[j]) + min(mGamma[i-1][j], mGamma[i][j-1], mGamma[i-1][j-1])
     double best = 1000000000;
     if (i > 0)
-          best = mGamma[(i - 1) * N + j];
+      best = mGamma[(i - 1) * N + j];
     if (j > 0)
       best = min(best, mGamma[i * N + j - 1]);
     if ((i > 0) && (j > 0))
       best = min(best, mGamma[(i - 1) * N + j - 1]);
     if ((i == 0) && (j == 0))
-      mGamma[i * N + j] = fabs(v[i] - w[j]);
+      mGamma[i * N + j] = fabs(shared_v[threadIdx.x] - shared_w[threadIdx.x]);
     else
-      mGamma[i * N + j] = best + fabs(v[i] - w[j]);
+      mGamma[i * N + j] = best + fabs(shared_v[threadIdx.x] - shared_w[threadIdx.x]);
     // printf("best = %f mGamma[%d][%d] = %f\n", best ,i, j, mGamma[i * N + j]);
   }
-
 
 dtw::dtw(uint n, uint constraint)
       :mGamma_origin(n ,std::vector<double>(n ,INF)) ,mN(n), mConstraint(constraint) {
@@ -50,7 +57,7 @@ dtw::dtw(uint n, uint constraint)
   init_mGamma<<<NBlks, K>>>(mGamma, size, std::numeric_limits<double>::infinity());
 
   // 等待GPU完成
-  cudaDeviceSynchronize();
+  checkCudaErrors(cudaDeviceSynchronize());
 };
 
 dtw::~dtw(){
@@ -58,51 +65,43 @@ dtw::~dtw(){
   mGamma_origin.clear();
 }
 
-double dtw::fastdynamic(const std::vector<double>& v, const std::vector<double>& w) {
+double dtw::fastdynamic(const double* v, const double* w) {
   if(!fast)
     return 0;
 
   cudaEvent_t start, stop;
-  cudaEventCreate(&start);
-  cudaEventCreate(&stop);
+  checkCudaErrors(cudaEventCreate(&start));
+  checkCudaErrors(cudaEventCreate(&stop));
 
   // 记录开始时间
-  cudaEventRecord(start);
+  checkCudaErrors(cudaEventRecord(start));
 
-  double* d_v;
-  checkCudaErrors(cudaMalloc(&d_v, v.size() * sizeof(double)));
-  checkCudaErrors(cudaMemcpy(d_v, v.data(), v.size() * sizeof(double), cudaMemcpyHostToDevice));
-  double* d_w;
-  checkCudaErrors(cudaMalloc(&d_w, w.size() * sizeof(double)));
-  checkCudaErrors(cudaMemcpy(d_w, w.data(), w.size() * sizeof(double), cudaMemcpyHostToDevice));
-
-  int N = v.size();
+  int N = mN;
   int K = 512; // 每个块的线程数
   int NBlks = ceil((float)N / K); // 计算所需的块数
   // 波前从0开始一直到2n  
-  for(int warefront = 0 ;warefront <= 2 * (N - 1) ;warefront++){
-    compute_DTW<<<NBlks,K>>>(d_v ,d_w ,mGamma ,warefront ,mConstraint ,mN);
-    std::cout << std::endl << "warefront = " << warefront << std::endl;
-    checkCudaErrors(cudaDeviceSynchronize());
+  for(int wavefront = 0 ;wavefront <= 2 * (N - 1) ;wavefront++){
+    compute_DTW<<<NBlks,K>>>(v ,w ,mGamma ,wavefront ,mConstraint ,mN);
+    // std::cout << std::endl << "warefront = " << warefront << std::endl;
+    // checkCudaErrors(cudaDeviceSynchronize());
     // for(int i = 0 ;i < N*N ;i++){
     //   std::cout << mGamma[i] << " ";
     // }
   }
 
   // 记录结束时间
-  cudaEventRecord(stop);
-  cudaEventSynchronize(stop);
+  checkCudaErrors(cudaEventRecord(stop));
+  checkCudaErrors(cudaEventSynchronize(stop));
 
   // 计算并打印执行时间
   float milliseconds = 0;
-  cudaEventElapsedTime(&milliseconds, start, stop);
+  checkCudaErrors(cudaEventElapsedTime(&milliseconds, start, stop));
   std::cout << "CUDA Execution time: " << milliseconds << " ms " << "fd: " << mGamma[(mN - 1) * N + mN - 1] << std::endl;
 
   // 清理
-  cudaEventDestroy(start);
-  cudaEventDestroy(stop);
-  checkCudaErrors(cudaFree(d_v));
-  checkCudaErrors(cudaFree(d_w));
+  checkCudaErrors(cudaEventDestroy(start));
+  checkCudaErrors(cudaEventDestroy(stop));
+
   return mGamma[(mN - 1) * N + mN - 1];
 }
 
@@ -111,11 +110,11 @@ double dtw::fastdynamic_origin(const std::vector<double>& v, const std::vector<d
     return 0;
 
   cudaEvent_t start, stop;
-  cudaEventCreate(&start);
-  cudaEventCreate(&stop);
+  checkCudaErrors(cudaEventCreate(&start));
+  checkCudaErrors(cudaEventCreate(&stop));
 
   // 记录开始时间
-  cudaEventRecord(start);
+  checkCudaErrors(cudaEventRecord(start));
 
   assert(static_cast<int>(v.size()) == mN);
   assert(static_cast<int>(w.size()) == mN);
@@ -144,16 +143,85 @@ double dtw::fastdynamic_origin(const std::vector<double>& v, const std::vector<d
     // printf("\n");
   }
   // 记录结束时间
-  cudaEventRecord(stop);
-  cudaEventSynchronize(stop);
+  checkCudaErrors(cudaEventRecord(stop));
+  checkCudaErrors(cudaEventSynchronize(stop));
 
   // 计算并打印执行时间
   float milliseconds = 0;
-  cudaEventElapsedTime(&milliseconds, start, stop);
+  checkCudaErrors(cudaEventElapsedTime(&milliseconds, start, stop));
   std::cout << "Origin Execution time: " << milliseconds << " ms " << "fd: " << mGamma_origin[mN - 1][mN - 1] << std::endl;
 
   // 清理
-  cudaEventDestroy(start);
-  cudaEventDestroy(stop);
+  checkCudaErrors(cudaEventDestroy(start));
+  checkCudaErrors(cudaEventDestroy(stop));
   return mGamma_origin[mN - 1][mN - 1];
+}
+
+// ------------------------------------------
+__global__ void compute_DTW_1(const double*v ,const double*w, double* mGamma, int wavefront, int constraint ,int N){
+    if(threadIdx.x != wavefront) 
+      return;
+
+    int i = blockIdx.x;
+    // 限制i的范围
+    if(i > wavefront || i > N) return;
+
+    // 超过constraint的不计算
+    int j = wavefront - i;
+    if(abs(i-j) > constraint  || j < 0 || j >= N) return;
+
+    printf("Thread [%d, %d]: i = %d, j = %d\n", blockIdx.x, threadIdx.x, i, j);
+
+    // 递推公式：mGamma[i][j] = fabs(v[i] - w[j]) + min(mGamma[i-1][j], mGamma[i][j-1], mGamma[i-1][j-1])
+    double best = 1000000000;
+    if (i > 0)
+      best = mGamma[(i - 1) * N + j];
+    if (j > 0)
+      best = min(best, mGamma[i * N + j - 1]);
+    if ((i > 0) && (j > 0))
+      best = min(best, mGamma[(i - 1) * N + j - 1]);
+    if ((i == 0) && (j == 0))
+      mGamma[i * N + j] = fabs(v[i] - w[j]);
+    else
+      mGamma[i * N + j] = best + fabs(v[i] - w[j]);
+    printf("best = %f mGamma[%d][%d] = %f\n", best ,i, j, mGamma[i * N + j]);
+  }
+
+double dtw::fastdynamic_1(const double* v, const double* w) {
+  if(!fast)
+    return 0;
+
+  cudaEvent_t start, stop;
+  checkCudaErrors(cudaEventCreate(&start));
+  checkCudaErrors(cudaEventCreate(&stop));
+
+  // 记录开始时间
+  checkCudaErrors(cudaEventRecord(start));
+
+  int max_wavefront = 2 * (mN - 1);
+  int threads_of_block = max_wavefront + 1;
+  int blocks_of_grid = mN * mN;
+  // 波前从0开始一直到2n  
+  for(int wavefront = 0 ;wavefront <= max_wavefront ;wavefront++){
+    std::cout << std::endl << "wavefront = " << wavefront << std::endl;
+    compute_DTW_1<<<blocks_of_grid ,threads_of_block>>>(v ,w ,mGamma ,wavefront ,mConstraint ,mN);
+    // for(int i = 0 ;i < N*N ;i++){
+    //   std::cout << mGamma[i] << " ";
+    // }
+  }
+
+  // 记录结束时间
+  checkCudaErrors(cudaEventRecord(stop));
+  checkCudaErrors(cudaEventSynchronize(stop));
+
+  // 计算并打印执行时间
+  float milliseconds = 0;
+  checkCudaErrors(cudaEventElapsedTime(&milliseconds, start, stop));
+  std::cout << "CUDA Execution time: " << milliseconds << " ms " << "fd: " << mGamma[(mN - 1) * mN + mN - 1] << std::endl;
+
+  // 清理
+  checkCudaErrors(cudaEventDestroy(start));
+  checkCudaErrors(cudaEventDestroy(stop));
+
+  return mGamma[(mN - 1) * mN + mN - 1];
 }
